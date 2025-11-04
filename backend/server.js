56d12cb4fea2afa79f02e7df2e87b5da8720b552
Saurabh Kumar <saurabhkuntal91@gmail.com>
2025-11-05 01:33:01 +0530

    resolve cors origin issue

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,7 @@ const aiRoutes = require('./routes/aiRoutes');
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
